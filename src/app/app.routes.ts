fddf5e53820a6f2b2038284fc2eed06cd70047cf
JKDoan <jennykimdoan@gmail.com>
2026-03-04 21:39:15 +0100

    added smooth scrolling to my werk

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ProjectItem } from './components/project-item/project-item';
 
 export const routes: Routes = [
     {
--- src/app/pages/onboarding/onboarding.ts
+++ src/app/pages/onboarding/onboarding.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
+import { Navbar } from '../../components/navbar/navbar';
+import { Title } from '../../components/article-items/title/title';
+import { ProjectDetails } from '../../components/article-items/project-details/project-details';
 
 @Component({
   selector: 'app-onboarding',
-  imports: [],
+  imports: [Footer, Navbar, Title, ProjectDetails],
   templateUrl: './onboarding.html',
 })
 export class Onboarding {
