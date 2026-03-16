ba3d3010bf23efc974a25b24ce56c87380ae2cc7
JKDoan <jennykimdoan@gmail.com>
2026-03-16 14:59:11 +0100

    done writing articles

--- src/app/pages/onboarding/onboarding.ts
+++ src/app/pages/onboarding/onboarding.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { Title } from '../../components/article-items/title/title';
 import { ProjectDetails } from '../../components/article-items/project-details/project-details';
 import { Paragraph } from '../../components/article-items/paragraph/paragraph';
-import { ParagraphWithImage } from '../../components/article-items/paragraph-with-image/paragraph-with-image';
 
 @Component({
   selector: 'app-onboarding',
-  imports: [Title, ProjectDetails, Paragraph, ParagraphWithImage],
+  imports: [Title, ProjectDetails, Paragraph],
   templateUrl: './onboarding.html',
 })
 export class Onboarding {
